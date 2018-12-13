52ff073be9ac1770e05688138b149a97d480fa2c
mushmoto <14163@hakodate.kosen-ac.jp>
2018-12-13 09:58:01 +0900

    Jhon追加（テスト）

---
+++ shark/Jhon.java
@@ -0,0 +1,23 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class Jhon here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Jhon extends Actor
+{
+    /**
+     * Act - do whatever the Jhon wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        if( Greenfoot.isKeyDown( "right" ) ){
+        setRotation(0);
+        move(1);
+    }
+        // Add your action code here.
+    }    
+}
