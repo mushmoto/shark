22f67038aab1730a4d0831b9ef9b39dff69bfd68
kzkmelon <kazu5206kazu@yahoo.co.jp>
2019-01-10 11:59:02 +0900

    World追加

---
+++ shark/Game_page.java
@@ -0,0 +1,21 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class Game_page here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Game_page extends World
+{
+
+    /**
+     * Constructor for objects of class Game_page.
+     * 
+     */
+    public Game_page()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
