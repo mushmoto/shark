2220e6ad5aeb00adcceaef197f2ba2f85035c272
nao0803 <14154@hakodate.kosen-ac.jp>
2019-01-17 10:43:35 +0900

    ゲーム画面にumi.png追加、ゲームオーバー画面に仮の文字を追加

--- shark/Game_over.java
+++ shark/Game_over.java
@@ -17,5 +17,7 @@ public class Game_over extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        showText("Game Over…", 200, 150);
+        showText(score, 400, 150);
     }
 }
