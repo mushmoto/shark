97550a0d099c2bbaaf7d44f7a4a8029f60ce22ec
nao0803 <14154@hakodate.kosen-ac.jp>
2019-01-31 11:53:26 +0900

    Game overロゴ作成

--- shark/Game_page.java
+++ shark/Game_page.java
@@ -19,7 +19,6 @@ public class Game_page extends World
     public static int getTime(){return time;}
     public static void addLife(){if(life != 2)life ++;}
 
-
     /**
      * Constructor for objects of class Game_page.
      * 
@@ -31,11 +30,8 @@ public class Game_page extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1, false);
         
-        
         addObject(new Score(), -10, -10);
         addObject(new Shark(), 70, 175);
-        
-        
         
         /*int exx =Greenfoot.getRandomNumber(getWidth()); 
         int exy =Greenfoot.getRandomNumber(getHeight()); */
@@ -52,23 +48,22 @@ public class Game_page extends World
         Greenfoot.setWorld(game);
     }
     
-
-    public void act(){
-        
+    public void act()
+    {    
             if(time <= 99999){time++;}
             else{ time = 0; }
             
             if(time % 20 == 0){
                 spawnEnemy();
             }
-   }
+    }
 
-    public void spawnEnemy(){
+    public void spawnEnemy()
+    {
         int enemyNum = Greenfoot.getRandomNumber(100);
         int place = Greenfoot.getRandomNumber(4);
         int spawn_x = 650;
         int spawn_y = 85+(90*place);
-        
         
         if(enemyNum >= 1 && enemyNum <= 40){
             addObject(new N_ter(), spawn_x, spawn_y);
@@ -81,12 +76,7 @@ public class Game_page extends World
         }else if(enemyNum >= 74 && enemyNum <= 86){
                 addObject(new Ex(), spawn_x, spawn_y);
         }else if(enemyNum >= 87 && enemyNum <= 100){
-                addObject(new Bomb(), spawn_x, spawn_y);
-        }
-        
-         
-        
-    }  
-
+                addObject(new Bomb(), spawn_x, spawn_y); 
+        }  
+    }
 }
-
